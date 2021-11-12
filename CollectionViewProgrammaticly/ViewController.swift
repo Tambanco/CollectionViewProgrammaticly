eaca86c12985c0673b7ddf15e4ae50759662c38c
Tambanco <52546246+Tambanco@users.noreply.github.com>
2021-11-12 13:14:40 +0300

    added headerView with multiplier

--- CollectionViewProgrammaticly/ViewController.swift
+++ CollectionViewProgrammaticly/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var headerView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initializeView()
     }
 
+    private func initializeView() {
+        headerView = UIView()
+        headerView.backgroundColor = #colorLiteral(red: 0.2941176471, green: 0.6431372549, blue: 0.9647058824, alpha: 1)
+        self.view.addSubview(headerView)
+        
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        headerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
+        headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.15).isActive = true
+        
+    }
 
 }
 

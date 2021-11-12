8643105dacf8fc236764d46391e43f1f05a2f98c
Tambanco <52546246+Tambanco@users.noreply.github.com>
2021-11-12 12:35:09 +0300

    Initial Commit

---
+++ CollectionViewProgrammaticly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewProgrammaticly
+//
+//  Created by tambanco 🥳 on 12.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
